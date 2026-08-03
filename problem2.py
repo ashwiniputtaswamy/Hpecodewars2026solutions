3d8084c9bd8a085fb46dba3ab37bd94516741590
Ashwini P <128336587+ashwiniputtaswamy@users.noreply.github.com>
2026-08-03 15:21:58 +0530

    Add student marks evaluation program
    Implement a student marks evaluation system that accepts a student's name and marks, calculates total, average, and assigns a grade based on performance.

---
+++ problem2.py
@@ -0,0 +1,59 @@
+--------------------------------------------------------------------------------------------------------------------------------------------------
+'''Problem 2:Student Marks System
+Description: A school wants to automate the process of evaluating student performance. 
+Write a program that accepts a student's name and their marks in 6 subjects, then computes their total marks, average, and assigns a grade basedon their performance.
+Below are the subjects for each student
+1. English
+2. Science
+3. Mathematics
+4. Social Science
+5. Regional Language
+6. Computer Science
+Ensure that the program does as below:
+1. Accept the student's name as input.
+2. Accept marks (0-100) for each of the 6 subjects.
+3. Calculate the total and average marks.
+4. Assign a grade based on the average.
+The overall grade for a student is calculated based on the average
+1. Average greater than or equal to 90 --> Grade A
+2. Average greater than or equal to 75 and below 90 --> Grade B
+3. Average greater than or equal to 50 and below 75 --> Grade C
+4. Average below 50 --> Fail
+Input:
+The first line consists of the name.
+The next six line contains the marks for each of the subjects
+Output:
+The first line should print the name of the student
+The second line should print "Total - "and the total marks obtained.
+The third line should print "Average -" and the average marks obtained, rounded to 2 decimal places.
+The fourth line should print "Grade - "and the grade secured.
+Constraints
+Marks for each subject is between 0 and 100.
+name contains only characters a-z and A-Z.
+Sample Input # 1
+Deepak
+40
+45
+38
+42
+41
+39
+Sample Output # 1
+Deepak
+Total - 245.00
+Average - 40.83
+Grade - Fail
+Sample Input # 2
+Elena
+70
+75
+80
+72
+78
+75
+Sample Output # 2
+Elena
+Total - 450.00
+Average - 75.00
+Grade - B'''
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
