0c05fce4ff7cb0964017ec8f5d92ae71754e6e2c
Ashwini P <128336587+ashwiniputtaswamy@users.noreply.github.com>
2026-08-03 14:33:24 +0530

    Add welcome message program for team input
    Implement program to welcome a team based on input number.

---
+++ problem0.py
@@ -0,0 +1,17 @@
+-----------------------------------------------------------------------------------------------------------------------------------------------
+'''Problem 0: Points To Give Away
+Description: The purpose os this question is to ensure your program can be executed correctly in the judge's computer.
+This is a variation of the classic program "Hello World!"
+The program should accept an input T representing the team number and then print the welcome message as shown in the Output section.
+Notice that there is nothing printed before accepting the input.
+Input: A single line consisting of team number T.
+Output: Print a single line containing "Welcome team T to the HPE Code Wars 2026! Cricket vs Football Theme!"
+Sample Input: 100
+Sample Output: Welcome team 100 to the HPE Code Wars 2026! Cricket vs Football Theme!'''
+--------------------------------------------------------------------------------------------------------------------------------------------------
+T = int(input(""))
+print("Welcome team",T,"to HPE Code Wars 2026! Cricket vs Football Theme!")
+--------------------------------------------------------------------------------------------------------------------------------------------------
+Output: 
+100
+Welcome team 100 to HPE Code Wars 2026! Cricket vs Football Theme!
