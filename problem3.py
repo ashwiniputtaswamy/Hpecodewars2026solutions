2c23c86ff1176234a6e8130fc803d7d710424857
Ashwini P <128336587+ashwiniputtaswamy@users.noreply.github.com>
2026-08-04 12:36:27 +0530

    Add voting system program in problem3.py
    Implement basic voting system for student council elections.

---
+++ problem3.py
@@ -0,0 +1,4 @@
+'''Problem 3: Basic Voting System
+Description: A school conducts student council elections every year and wants to digitize the vote-counting process.
+Write a program that reads the list of candidates and the votes cast by students, calculates the percentage of votes received by each candidate, and determines the winner.
+In the election, multiple candidates contest, and each student votes for one candidate'''
